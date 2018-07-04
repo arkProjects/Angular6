acf9fddc5dfffc9293a0bb56f049588fcdfae9a4
admin <admin@admins-MacBook-Pro.local>
2018-07-04 19:11:59 +0530

    end of day 1

---
+++ pre-requisites/02-typescript-basics/session/dist/02-primitives.js
@@ -0,0 +1,7 @@
+(function () {
+    var hotelId = 123;
+    var hotelName = 'A2B'; // type is optional
+    var name = 'eRevMax';
+    var isRaining = false;
+    hotelId = 'Abc123'; // TypeScript automatically infers type
+}());
