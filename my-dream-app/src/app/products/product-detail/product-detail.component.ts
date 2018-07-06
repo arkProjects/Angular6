a8a7bfc9512bd08dfa1d0cf6bf8ea25a00225464
admin <admin@admins-MacBook-Pro.local>
2018-07-06 13:35:30 +0530

    upto lunch on day 3 (most of routing done)

--- my-dream-app/src/app/products/product-detail/product-detail.component.ts
+++ my-dream-app/src/app/products/product-detail/product-detail.component.ts
@@ -2,17 +2,36 @@ import { Component, OnInit } from '@angular/core';
 import productsArray from '../products';
 import IProduct from '../Product';
 
+import { ActivatedRoute, Router } from '@angular/router';
+
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
+    productId: number;
     product : IProduct;
 
-    constructor() { }
+    constructor( private _activatedRoute: ActivatedRoute, private _router: Router ) {
+        console.log( _activatedRoute );
+    }
 
     ngOnInit() {
-        this.product = productsArray[0];
+        // all params are strings
+        // this.productId = +this._activatedRoute.snapshot.params.id;
+        // paramMap is an Observable (not part of ANgular - comes from Rxjs)
+        // An Observable has a method called subscribe - 3 arguments (all functions)
+        this._activatedRoute.paramMap.subscribe(
+            params => {
+                this.productId = +params.get( 'id' );
+                this.product = productsArray[this.productId - 1];
+            }
+        );
+    }
+
+    navigateToProductList() {
+        // this._router.navigate( [ '/products' ] );
+        this._router.navigateByUrl( '/products' );
     }
 }
