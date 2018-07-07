ae5196ce910c7313d913436b1e6ca9d6ebb9aca5
admin <admin@admins-MacBook-Pro.local>
2018-07-07 11:37:15 +0530

    upto getProducts() implementation on day 4

--- my-dream-app/src/app/products/product-list/product-list.component.ts
+++ my-dream-app/src/app/products/product-list/product-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductsService } from '../products.service';
 
+import { Subscription } from 'rxjs';
+
 import IProduct from '../Product';
 import productsArray from '../products';
 
@@ -10,27 +12,33 @@ import productsArray from '../products';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
-    products: any;
+    products: IProduct[];
     filteredProducts: IProduct[];
     isImageVisible: boolean = true;
     filterKey: string = '';
 
+    productsSubscription: Subscription;
+
     constructor( private _productsService: ProductsService ) {
-        
     }
 
     // called when component has been added to DOM
     ngOnInit() {
-        this._productsService.getProducts().subscribe(
+        this.productsSubscription = this._productsService.getProducts().subscribe(
             products => {
                 this.products = products;
                 this.filteredProducts = this.products;
-            }
+            },
+            ( err ) => console.log( err )
         );
     }
 
     toggleImage() {
         this.isImageVisible = !this.isImageVisible;
+    }
+
+    ngOnDestroy() {
+        this.productsSubscription.unsubscribe();
     }
 
     filterProducts() {
--- my-dream-app/src/app/products/products.service.ts
+++ my-dream-app/src/app/products/products.service.ts
@@ -4,13 +4,15 @@ import { environment } from '../../environments/environment';
 
 import IProduct from './products';
 
+import { Observable } from 'rxjs';
+
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
-  constructor( private _httpClient: HttpClient ) { }
+    constructor( private _httpClient: HttpClient ) { }
 
-  getProducts()  {
-      return this._httpClient.get( environment.apiBaseUrl + 'products' );
-  }
+    getProducts() : Observable<any> {
+        return this._httpClient.get( environment.apiBaseUrl + 'products' ); // an observable with only one event (single HttpResponse)
+    }
 }
